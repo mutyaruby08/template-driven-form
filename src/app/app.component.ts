ed8b164d2c2ebd6f513ea24e9b2a0751745f5f94
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2021-10-17 15:04:09 +0800

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form';
+  @ViewChild('myForm') signUpForm!: NgForm;
+  genders = ["Male", "Female", "LGBTQ+", "Prefer not to say"]
+  submitted = false;
+  default = 'pet';
+  user = {
+    username: '', password: '', email: '', phone: '', secret: '', ans: '',
+    gender: '', job: '', jobDes: '', salary: ''
+  }
+
+
+
+  onSubmit(){
+    this.submitted  = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.password = this.signUpForm.value.userData.password;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.phone = this.signUpForm.value.userData.phone;
+    this.user.secret = this.signUpForm.value.otherInfo.secret;
+    this.user.ans = this.signUpForm.value.otherInfo.ans;
+    this.user.gender = this.signUpForm.value.otherInfo.gender;
+    this.user.job = this.signUpForm.value.otherInfo.job;
+    this.user.jobDes = this.signUpForm.value.otherInfo.jobDes;
+    this.user.salary = this.signUpForm.value.otherInfo.salary;
+
+    this.signUpForm.reset();
+
+
+  }
 }
